142c6e7886e18790a3ab32923b562186ce1144bf
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2021-07-03 09:44:31 +0530

    make following asterisk pattern
    *
    **
    ***
    ****

---
+++ asterisk_pattern.cpp
@@ -0,0 +1,51 @@
+/*
+
+PROBLEM =>
+Print the following patterns using loop :
+
+*
+**
+***
+****
+
+CODER =>
+Mohd Mehdi
+
+DATE =>
+29/06/2021
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for (int i = 1; i <= 1; i++)
+    {
+        cout << "*";
+    }
+    cout <<"\n";
+    for (int j = 1; j <= 2; j++)
+    {
+        cout << "*";
+    }
+    cout <<"\n";
+    for (int k = 1; k <= 3; k++)
+    {
+        cout << "*";
+    }
+    cout <<"\n";
+    for (int l = 1; l <= 4; l++)
+    {
+        cout << "*";
+    }
+    
+    
+    
+    return 0;
+}
+
+    
+
